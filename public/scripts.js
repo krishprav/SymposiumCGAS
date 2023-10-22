ec00d909ea37d3adf44e026c353d1e8397f61ffb
vyshnav20157 <vyshnav20157@iiitd.ac.in>
2023-10-22 17:20:40 +0300

    Added logo. Made navbar trans. Added JS file.

---
+++ public/scripts.js
@@ -0,0 +1,35 @@
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+// Change style of navbar on scroll
+window.addEventListener('scroll', function() {
+  var navbar = document.getElementById('myNavbar');
+  var aboutSection = document.getElementById('about').offsetTop;
+
+  if (window.scrollY >= aboutSection) {
+    navbar.style.opacity = "1"; // 100% opacity
+  } else {
+    navbar.style.opacity = "0.65"; // 65% opacity
+  }
+});
