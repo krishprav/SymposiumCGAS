b0a6d9786b2d5769c9f6391b8d329e23abdf6976
vyshnav20157 <vyshnav20157@iiitd.ac.in>
2023-10-23 15:08:22 +0300

    thumbs up

--- public/scripts.js
+++ public/scripts.js
@@ -21,3 +21,24 @@ function w3_open() {
 function w3_close() {
     mySidebar.style.display = "none";
 }
+
+// Setting the height of the hero section
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the height of the navbar
+  const navbarHeight = document.getElementById('myNavbar').offsetHeight;
+  // Get the hero section
+  const heroSection = document.querySelector('.bgimg-1');
+  // Set the top and height of the hero section
+  heroSection.style.top = `${navbarHeight}px`;
+  heroSection.style.height = `calc(100vh - ${navbarHeight}px)`;
+
+  // Get the button by its id
+  const learnMoreBtn = document.getElementById("learnMore");
+  // Get the target section by its id
+  const aboutSection = document.getElementById("about");
+  // Attach a click event listener to the button
+  learnMoreBtn.addEventListener("click", function() {
+    // Use scrollIntoView to scroll to the target section
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
